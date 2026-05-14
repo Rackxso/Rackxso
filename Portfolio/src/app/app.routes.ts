8456ab46d6198de1b6dd848d3546eafab512d890
Rackxso <oscarurenas04@gmail.com>
2026-05-14 16:39:42 +0200

    Estructura con editor completa

--- Portfolio/src/app/app.routes.ts
+++ Portfolio/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./components/home/home').then(m => m.Home)
+    },
+    {
+        path: 'about',
+        loadComponent: () => import('./components/about/about').then(m => m.About)
+    },
+    {
+        path: 'skill',
+        loadComponent: () => import('./components/skills/skills').then(m => m.Skills)
+    },
+    {
+        path: 'projects',
+        loadComponent: () => import('./components/projects/projects').then(m => m.Projects)
+    },
+    {
+        path: 'contact',
+        loadComponent: () => import('./components/contact/contact').then(m => m.Contact)
+    },
+];
--- Portfolio/src/app/app.ts
+++ Portfolio/src/app/app.ts
@@ -1,12 +1,39 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { NgFor, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Portfolio');
+
+  public openedTabs: string[] = [];
+  public activeTab: string = '';
+
+  constructor(private router: Router) {}
+
+  public redirigir(pagina: string) {
+    if (!this.openedTabs.includes(pagina)) {
+      this.openedTabs.push(pagina);
+    }
+    this.activeTab = pagina;
+    this.router.navigate([`/${pagina}`]);
+  }
+
+  public closeTab(pagina: string, event: MouseEvent) {
+    event.stopPropagation();
+    const index = this.openedTabs.indexOf(pagina);
+    this.openedTabs.splice(index, 1);
+
+    if (this.activeTab === pagina) {
+      const newActive = this.openedTabs[index - 1] ?? this.openedTabs[0] ?? null;
+      this.activeTab = newActive ?? '';
+      if (newActive) this.router.navigate([`/${newActive}`]);
+      else this.router.navigate(['/']);
+    }
+  }
 }
